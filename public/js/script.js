22c26a280928598cb36d5e621b71abc9d063c785
Mariia Kahlert <mariia.kahlert@gmail.com>
2021-04-30 11:50:13 +0200

    starts writing functionality for upload of an image

--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,32 @@ new Vue({
     el: "#main",
     data: {
         images: [],
+        description: "",
+        username: "",
+        title: "",
+        file: null,
     },
     mounted: function () {
         axios.get("/images").then((response) => {
             this.images = response.data;
         });
+    },
+    methods: {
+        handleChange: function (e) {
+            console.log("handleChange is running");
+            this.file = e.target.files[0];
+        },
+        submitFile: function () {
+            console.log("submitFile is running");
+            var formData = new FormData();
+            formData.append("file", this.file);
+            formData.append("title", this.title);
+            formData.append("description", this.description);
+            formData.append("username", this.username);
+            axios
+                .post("/upload", formData)
+                .then(() => {})
+                .catch((err) => console.log(err));
+        },
     },
 });
--- server.js
+++ server.js
@@ -4,12 +4,54 @@ const app = express();
 
 app.use(express.static("public"));
 
+// Beginning of the code required to upload files
+
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+
+// End of the code required to upload files
+
 app.use("/images", (req, res) => {
     selectAllImages()
         .then((result) => {
             res.json(result.rows);
         })
         .catch((err) => console.log(err));
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log("Upload worked!");
+    console.log(req.body); // text inputs
+    console.log(req.file); // file
+    if (req.file) {
+        // send back a response to Vue using res.json
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8080, () => console.log("Server listening on port 8080"));
